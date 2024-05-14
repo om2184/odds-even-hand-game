3d0a7fb2cd6394f0589e0bd98676da890f39f4b7
Om Patel <opat597@aucklanduni.ac.nz>
2024-05-14 21:47:19 +1200

    Says round number when PLAY command used. Task 2 passed

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,12 +6,17 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  private int roundNumber = 1;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
-  public void play() {}
+  public void play() {
+    MessageCli.START_ROUND.printMessage(String.valueOf(roundNumber));
+    roundNumber++;
+  }
 
   public void endGame() {}
 
