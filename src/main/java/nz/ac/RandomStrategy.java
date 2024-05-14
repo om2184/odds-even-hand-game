c2e3c9f3e8d8d058ca05a42d19373ef0f71fc8f5
Om Patel <opat597@aucklanduni.ac.nz>
2024-05-14 22:32:13 +1200

    create Ai interface and class for easy difficulty

--- src/main/java/nz/ac/RandomStrategy.java
+++ src/main/java/nz/ac/RandomStrategy.java
@@ -1,10 +1,12 @@
 package nz.ac;
 
+import nz.ac.auckland.se281.Utils;
+
 public class RandomStrategy implements Strategy {
 
   @Override
-  public void newGame() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'newGame'");
+  public void aiNumber() {
+    // This strategy selects the fingers to play randomly between 0 and 5.
+    int random = Utils.getRandomNumberRange(0, 5);
   }
 }
