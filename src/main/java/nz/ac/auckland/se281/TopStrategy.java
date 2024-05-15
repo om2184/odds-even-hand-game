b959d016191d6f0d25cbb2fa3e5fc37a66a7177a
Om Patel <opat597@aucklanduni.ac.nz>
2024-05-15 20:20:29 +1200

    created medium level and top strategy files

---
+++ src/main/java/nz/ac/auckland/se281/TopStrategy.java
@@ -0,0 +1,10 @@
+package nz.ac.auckland.se281;
+
+public class TopStrategy implements Strategy {
+
+  @Override
+  public int aiNumber() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'aiNumber'");
+  }
+}
